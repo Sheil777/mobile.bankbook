b3a4e4acf7311c400402897eb5940865ca9e5412
Sheil <karaulov.z@mail.ru>
2025-07-08 21:55:43 +0300

    Добавлена шапка

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,28 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center",
       }}
     >
+      <View 
+        style={{
+          width: '85%',
+          marginTop: 10,
+        }}
+      >
+        <Image 
+          source={require('../assets/images/logo.png')}
+          style={{
+            width: '100%',
+          }}
+          resizeMode="contain"
+        />
+      </View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
